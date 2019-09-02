c67b5a25cee31dd3c886b640c9d24594f09b4c2b
ankitjain28may <ankitjain28may77@gmail.com>
2019-09-02 18:55:17 +0530

    fixing context issue

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f /home/ubuntu/docker/personal-blog/docker-compose.yml run certbot renew --dry-run \
-&& /usr/local/bin/docker-compose -f /home/ubuntu/docker/personal-blog/docker-compose.yml kill -s SIGHUP nginx
+cd /home/ubuntu/docker/personal-blog/ \
+&& /usr/local/bin/docker-compose -f docker-compose.yml run certbot renew --dry-run \
+&& /usr/local/bin/docker-compose -f docker-compose.yml kill -s SIGHUP nginx
